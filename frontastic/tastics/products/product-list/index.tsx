df5e98515c58f1f21784cf47e0fe986af82dd49e
Mina Badrous <minabadrous.vu@gmail.com>
2022-03-20 13:59:49 +0200

    Fixed empty PL fallback concept

--- frontastic/tastics/products/product-list/index.tsx
+++ frontastic/tastics/products/product-list/index.tsx
@@ -3,7 +3,7 @@ import ProductList from 'components/frontastic-ui/products/product-list';
 function ProductListTastic({ data }) {
   const { items } = data.data.dataSource;
 
-  if (!items) return <p>No products found.</p>;
+  if (!items || items.length == 0) return <p>No products found.</p>;
 
   return <ProductList products={items} />;
 }
