4b45822cb292a4bafbdd67f8440e2fe91810cbdb
James Morrison <james@frontastic.cloud>
2022-03-28 07:29:45 +0100

    Added action return result to mutate

--- frontastic/actions/wishlist.ts
+++ frontastic/actions/wishlist.ts
@@ -6,16 +6,16 @@ export const getWishlist = async () => {
 }
 
 export const addToWishlist = async ({ sku, count = 1 }: { sku: string; count?: number }) => {
-    fetchApiHub('/action/wishlist/addToWishlist', { method: 'POST' }, { variant: sku, count });
-    mutate('/action/wishlist/getWishlist');
+    const res = await fetchApiHub('/action/wishlist/addToWishlist', { method: 'POST' }, { variant: sku, count });
+    mutate('/action/wishlist/getWishlist', res);
 }
 
 export const removeLineItem = async (lineItemId: string) => {
-    fetchApiHub('/action/wishlist/removeLineItem', { method: 'DELETE' }, { lineItem: { id: lineItemId } });
-    mutate('/action/wishlist/getWishlist');
+    const res = await fetchApiHub('/action/wishlist/removeLineItem', { method: 'POST' }, { lineItem: { id: lineItemId } });
+    mutate('/action/wishlist/getWishlist', res);
 }
 
 export const updateLineItem = async ({ lineItemId, count = 1 }: { lineItemId: string; count?: number }) => {
-    fetchApiHub('/action/wishlist/updateLineItemCount', { method: 'PUT' }, { lineItem: { id: lineItemId }, count });
-    mutate('/action/wishlist/getWishlist');
+    const res = await fetchApiHub('/action/wishlist/updateLineItemCount', { method: 'POST' }, { lineItem: { id: lineItemId }, count });
+    mutate('/action/wishlist/getWishlist', res);
 }
