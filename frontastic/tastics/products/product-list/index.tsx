b0dcabb4ecfbd692cb0a79717008c766b40d222f
Andrius <xcintas@gmail.com>
2022-04-04 13:48:05 +0300

    product listing tastic fix

--- frontastic/tastics/products/product-list/index.tsx
+++ frontastic/tastics/products/product-list/index.tsx
@@ -1,6 +1,10 @@
 import ProductList from 'components/frontastic-ui/products/product-list';
 
-function ProductListTastic({ data }) {
+function ProductListTastic({ data }) {  
+  if (!data?.data?.dataSources?.__master) {
+    return null
+  }
+
   const { products, category, nextCursor, previousCursor } = data.data.dataSources.__master;
 
   if (!products || products.length == 0) return <p>No products found.</p>;
