ff4cf753689d274a8ceda8b74153b8d1e4db5081
jim <jim@frontastic.cloud>
2022-04-21 12:15:37 +0300

    fix: Fix the default locale for nextjs frontend

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   i18n: {
-    defaultLocale: 'de_CH',
+    defaultLocale: 'en_GB',
     locales: ['en_GB', 'de_CH'],
   },
   localePath: path.resolve('./public/locales'),
