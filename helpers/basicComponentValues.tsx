f84489b10ab409760b870d1d64d93efc661fcf1a
Sven Koschnicke <s.koschnicke@gfxpro.com>
2022-05-04 16:07:49 +0200

    feat: provide types for component values

---
+++ helpers/basicComponentValues.tsx
@@ -0,0 +1,10 @@
+// see https://docs.frontastic.cloud/docs/frontastic-schemas#schema-field-types
+export type ComponentValueString = string;
+export type ComponentValueText = string;
+export type ComponentValueMarkdown = string;
+export type ComponentValueNumber = number;
+export type ComponentValueInteger = number;
+export type ComponentValueDecimal = number;
+export type ComponentValueEnum = string;
+export type ComponentValueBoolean = boolean;
+export type ComponentValueJson = string;
--- helpers/reference.tsx
+++ helpers/reference.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import NextLink from 'next/link';
 
+/* This interface describes the data structure a component gets back for fields
+ * of type "reference" in the component schema, when an external link is
+ * configured in the studio. See
+ * https://docs.frontastic.cloud/docs/frontastic-schemas#schema-field-types
+ */
 interface LinkReference {
   type: 'link';
   link: string;
@@ -8,6 +13,11 @@ interface LinkReference {
   openInNewWindow: boolean;
 }
 
+/* This interface describes the data structure a component gets back for fields
+ * of type "reference" in the component schema, when a page folder is configured
+ * in the studio. See
+ * https://docs.frontastic.cloud/docs/frontastic-schemas#schema-field-types
+ */
 interface PageFolderReference {
   type: 'page-folder';
   pageFolder: {
