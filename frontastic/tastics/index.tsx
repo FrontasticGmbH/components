638cbf7b65f6c36f2c1b97e1435ba3963c41012b
Andrius <xcintas@gmail.com>
2022-05-09 10:45:19 +0300

    ordered tastic imports

--- frontastic/tastics/index.tsx
+++ frontastic/tastics/index.tsx
@@ -1,3 +1,7 @@
+// Doc Examples
+import SimpleButton from './doc-examples/simple-button';
+import StarWarsCharacterSearch from './doc-examples/star-wars/character-search';
+
 import NotFound from './not-found';
 import Footer from './footer';
 import Header from './header';
@@ -6,22 +10,19 @@ import Checkout from './checkout';
 import CheckoutSuccess from './checkout-success';
 import Markdown from './content/markdown';
 import Tile from './content/tile';
+import SpacerTastic from './content/spacer';
 import Newsletter from './newsletter';
 import ProductDetails from './products/details';
 import ProductList from './products/product-list';
 import ProductSlider from './products/slider';
+import WishlistTastic from './wishlist';
+import ShowcaseTastic from './showcase';
 import AccountTest from './account/test';
 import AccountOrdersHistoryTastic from './account/orders';
-// Doc Examples
-import SimpleButton from './doc-examples/simple-button';
-import StarWarsCharacterSearch from './doc-examples/star-wars/character-search';
 import AccountDetailsTastic from './account/details';
 import AccountLoginTastic from './account/login';
 import AccountRegisterTastic from './account/register';
-import SpacerTastic from './content/spacer';
-import WishlistTastic from './wishlist';
 import ResetPasswordTastic from './account/reset-password';
-import ShowcaseTastic from './showcase';
 
 export const tastics = {
   'frontastic/ui/checkout': Checkout,
