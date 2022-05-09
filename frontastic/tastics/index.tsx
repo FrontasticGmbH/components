f9c2079555578765b8fef91e9575088177b2909d
Andrius <xcintas@gmail.com>
2022-05-09 10:48:09 +0300

    renamed tastic imports

--- frontastic/tastics/index.tsx
+++ frontastic/tastics/index.tsx
@@ -10,19 +10,19 @@ import Checkout from './checkout';
 import CheckoutSuccess from './checkout-success';
 import Markdown from './content/markdown';
 import Tile from './content/tile';
-import SpacerTastic from './content/spacer';
+import Spacer from './content/spacer';
+import Showcase from './showcase';
 import Newsletter from './newsletter';
 import ProductDetails from './products/details';
 import ProductList from './products/product-list';
 import ProductSlider from './products/slider';
-import WishlistTastic from './wishlist';
-import ShowcaseTastic from './showcase';
+import Wishlist from './wishlist';
 import AccountTest from './account/test';
-import AccountOrdersHistoryTastic from './account/orders';
-import AccountDetailsTastic from './account/details';
-import AccountLoginTastic from './account/login';
-import AccountRegisterTastic from './account/register';
-import ResetPasswordTastic from './account/reset-password';
+import AccountOrdersHistory from './account/orders';
+import AccountDetails from './account/details';
+import AccountLogin from './account/login';
+import AccountRegister from './account/register';
+import ResetPassword from './account/reset-password';
 
 export const tastics = {
   'frontastic/ui/checkout': Checkout,
@@ -30,22 +30,22 @@ export const tastics = {
   'frontastic/ui/cart': Cart,
   'frontastic/ui/footer': Footer,
   'frontastic/ui/header': Header,
-  'frontastic/ui/content/markdown': Markdown,
   'frontastic/ui/content/tile': Tile,
+  'frontastic/ui/content/spacer': Spacer,
+  'frontastic/ui/content/showcase': Showcase,
+  'frontastic/ui/content/markdown': Markdown,  
   'frontastic/ui/content/newsletter': Newsletter,
   'frontastic/ui/products/details': ProductDetails,
   'frontastic/ui/products/product-list': ProductList,
-  'frontastic/ui/products/slider': ProductSlider,
-  'frontastic/ui/account/test': AccountTest,
+  'frontastic/ui/products/slider': ProductSlider,  
+  'frontastic/ui/wishlist': Wishlist,
   'frontastic/ui/examples/simple-button': SimpleButton,
   'frontastic/ui/examples/star-wars/character-search': StarWarsCharacterSearch,
-  'frontastic/ui/account/details': AccountDetailsTastic,
-  'frontastic/ui/account/login': AccountLoginTastic,
-  'frontastic/ui/account/register': AccountRegisterTastic,
-  'frontastic/ui/account/orders': AccountOrdersHistoryTastic,
-  'frontastic/ui/content/spacer': SpacerTastic,
-  'frontastic/ui/wishlist': WishlistTastic,
-  'frontastic/ui/account/reset-password': ResetPasswordTastic,
-  'frontastic/ui/content/showcase': ShowcaseTastic,
+  'frontastic/ui/account/test': AccountTest,
+  'frontastic/ui/account/details': AccountDetails,
+  'frontastic/ui/account/login': AccountLogin,
+  'frontastic/ui/account/register': AccountRegister,
+  'frontastic/ui/account/orders': AccountOrdersHistory,
+  'frontastic/ui/account/reset-password': ResetPassword,  
   default: NotFound,
 };
