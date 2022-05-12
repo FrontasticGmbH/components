48e25520d747a4d60c3eb9b8147f0269fc7b4ce6
KenwayTheGreat <ahmad_amir@live.com>
2022-05-12 16:03:47 +0200

    Structured Tailwind Classes

--- components/frontastic-ui/header/cart-button.tsx
+++ components/frontastic-ui/header/cart-button.tsx
@@ -13,22 +13,19 @@ const CartButton: React.FC<CartButtonProps> = ({ cartItemCount, cartLink }) => {
   //i18n messages
   const { formatMessage: formatCartMessge } = useFormat({ name: 'cart' });
 
-  const cartButtonClassNames = {
-    'cart-btn': 'flow-root',
-    'cart-btn__wrap': 'group relative -m-2 flex items-center p-2',
-    'cart-btn__icon': 'h-6 w-6 flex-shrink-0 text-primary-400 group-hover:text-primary-500',
-    'cart-btn__badge': 'absolute -top-[-1px] -right-[2px] h-4 w-4 rounded-full bg-accent-400 hover:bg-accent-500',
-    'cart-btn__badge-text':
-      'font-small flex h-full w-full items-center justify-center text-[12px] text-white group-hover:text-white',
-  };
   return (
-    <div className={classNames(cartButtonClassNames['cart-btn'])}>
-      <ReferenceLink target={cartLink} className={classNames(cartButtonClassNames['cart-btn__wrap'])}>
-        <ShoppingCartIcon className={classNames(cartButtonClassNames['cart-btn__icon'])} aria-hidden="true" />
+    <div className="flow-root">
+      <ReferenceLink target={cartLink} className="group relative -m-2 flex items-center p-2">
+        <ShoppingCartIcon
+          className="h-6 w-6 shrink-0 text-primary-400 group-hover:text-primary-500"
+          aria-hidden="true"
+        />
         {cartItemCount > 0 && (
           <>
-            <span className={classNames(cartButtonClassNames['cart-btn__badge'])}>
-              <span className={classNames(cartButtonClassNames['cart-btn__badge-text'])}>{cartItemCount}</span>
+            <span className="absolute -top-[-1px] -right-[2px] h-4 w-4 rounded-full bg-accent-400 hover:bg-accent-500">
+              <span className="font-small flex h-full w-full items-center justify-center text-[12px] text-white group-hover:text-white">
+                {cartItemCount}
+              </span>
             </span>
             <span className="sr-only">
               {formatCartMessge({
--- components/frontastic-ui/header/wishlist-button.tsx
+++ components/frontastic-ui/header/wishlist-button.tsx
@@ -13,22 +13,14 @@ const WishListButton: React.FC<WishListButtonProps> = ({ wishlistItemCount, wish
   //i18n messages
   const { formatMessage: formatWishlistMessage } = useFormat({ name: 'wishlist' });
 
-  const wishlistButtonClassNames = {
-    'wishlist-btn': 'flow-root pr-3',
-    'wishlist-btn__wrap': 'group relative -m-2 flex items-center p-2',
-    'wishlist-btn__icon': 'h-6 w-6 flex-shrink-0 text-primary-400 group-hover:text-primary-500',
-    'wishlist-btn__badge': 'absolute -top-[-1px] -right-[2px] h-4 w-4 rounded-full bg-accent-400 hover:bg-accent-500',
-    'wishlist-btn__badge-text':
-      'font-small flex h-full w-full items-center justify-center text-[12px] text-white group-hover:text-white',
-  };
   return (
-    <div className={classNames(wishlistButtonClassNames['wishlist-btn'])}>
-      <ReferenceLink target={wishlistLink} className={classNames(wishlistButtonClassNames['wishlist-btn__wrap'])}>
-        <HeartIcon className={classNames(wishlistButtonClassNames['wishlist-btn__icon'])} aria-hidden="true" />
+    <div className="flow-root pr-3">
+      <ReferenceLink target={wishlistLink} className="group relative -m-2 flex items-center p-2">
+        <HeartIcon className="h-6 w-6 shrink-0 text-primary-400 group-hover:text-primary-500" aria-hidden="true" />
         {wishlistItemCount > 0 && (
           <>
-            <span className={classNames(wishlistButtonClassNames['wishlist-btn__badge'])}>
-              <span className={classNames(wishlistButtonClassNames['wishlist-btn__badge-text'])}>
+            <span className="absolute -top-[-1px] -right-[2px] h-4 w-4 rounded-full bg-accent-400 hover:bg-accent-500">
+              <span className="font-small flex h-full w-full items-center justify-center text-[12px] text-white group-hover:text-white">
                 {wishlistItemCount}
               </span>
             </span>
