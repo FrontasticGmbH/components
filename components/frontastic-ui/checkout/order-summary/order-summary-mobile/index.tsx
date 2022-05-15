7108ab5bd11a4f5a5e27f7b04e92d4f6b6da1ebd
KenwayTheGreat <ahmad_amir@live.com>
2022-05-15 13:54:28 +0200

    Removed Commented Code: Removed Commented but please review if that code is needed.

--- components/frontastic-ui/checkout/order-summary/order-summary-mobile/index.tsx
+++ components/frontastic-ui/checkout/order-summary/order-summary-mobile/index.tsx
@@ -116,25 +116,6 @@ const MobileOrderSummary = ({
                     </p>
                   )}
                 </ul>
-                {/*<form className="mt-10">
-                                <label htmlFor="discount-code-mobile" className="block text-sm font-medium text-gray-700">
-                                    Discount code
-                                </label>
-                                <div className="flex space-x-4 mt-1">
-                                    <input
-                                        type="text"
-                                        id="discount-code-mobile"
-                                        name="discount-code-mobile"
-                                        className="block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                                    />
-                                    <button
-                                        type="submit"
-                                        className="bg-gray-200 text-sm font-medium text-gray-600 rounded-md px-4 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500"
-                                    >
-                                        Apply
-                                    </button>
-                                </div>
-                                                        </form>*/}
 
                 <dl className="mt-10 space-y-6 text-sm font-medium text-gray-500">
                   <div className="flex justify-between">
