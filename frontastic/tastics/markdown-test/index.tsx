517bdd5cc3ebc420b6a6abe16ebbbc218c1ee570
Sara Samain <saragsamain@gmail.com>
2022-06-08 10:40:46 +0200

    fix (FCL-168): Rename tastic schema names to lowercase

--- frontastic/tastics/markdown-test/index.tsx
+++
@@ -1,7 +0,0 @@
-import Markdown from 'components/commercetools-ui/content/markdown';
-
-const MarkdownTastic = ({ data }) => {
-  return <Markdown text={data.markdown} />;
-};
-
-export default MarkdownTastic;
