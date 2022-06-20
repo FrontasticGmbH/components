b60b72357aae3bb83626a2a76932c973646ac0b0
Sara Samain <saragsamain@gmail.com>
2022-06-20 08:16:20 +0200

    bugfix(FCL-147): Filter titles not theme switching friendly

--- components/commercetools-ui/filters/SortingDisclosure.tsx
+++ components/commercetools-ui/filters/SortingDisclosure.tsx
@@ -44,7 +44,7 @@ const SortingDisclosure: FC<SortingDisclosureProps> = ({ updateSortingParams }) 
       <Disclosure>
         {({ open }) => (
           <>
-            <Disclosure.Button className="flex w-full items-center justify-between bg-white py-3 text-gray-400 hover:text-gray-500 dark:bg-primary-400">
+            <Disclosure.Button className="flex w-full items-center justify-between py-3 text-gray-400 hover:text-gray-500">
               <span className="font-medium text-neutral-600 dark:text-light-100">
                 {formatMessage({ id: 'sortBy', defaultMessage: 'Sort by' })}
               </span>
