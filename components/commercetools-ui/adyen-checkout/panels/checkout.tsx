83ec45bead9c56b197d0e095c9c4b47b91746a36
Bruno Jesus <bruno.fl.jesus@gmail.com>
2022-06-27 15:12:30 +0100

    fix: ESLint error

--- components/commercetools-ui/adyen-checkout/panels/checkout.tsx
+++ components/commercetools-ui/adyen-checkout/panels/checkout.tsx
@@ -27,11 +27,7 @@ const Checkout = () => {
   useEffect(() => {
     const host = typeof window !== 'undefined' ? window.location.origin : '';
 
-    createSession(
-      cartList.sum.centAmount,
-      cartList.sum.currencyCode,
-      `${host}/thank-you`,
-    ).then((res) => {
+    createSession(cartList.sum.centAmount, cartList.sum.currencyCode, `${host}/thank-you`).then((res) => {
       const { id, sessionData } = res;
 
       setSession({ id, sessionData });
