742b26c4e3bfdc5edf22b1c018bc5e7c1713034e
abdotaker608 <abdotaker607@gmail.com>
2022-06-28 11:30:20 +0200

    Fixed multiple requests on email verification

--- pages/verify/index.tsx
+++ pages/verify/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import Toast from 'react-hot-toast';
@@ -18,40 +18,35 @@ const Verify: NextPage = () => {
   //account actions
   const { confirm } = useAccount();
 
+  //successful redirection after verification
+  const successRedirect = useCallback(() => {
+    router
+      .push('/')
+      .then(() => Toast.success(formatAccountMessage({ id: 'verification.done', defaultMessage: 'Email verified' })));
+  }, []);
+
+  //error redirection becaues of invalid token
+  const errorRedirect = useCallback(() => {
+    router
+      .push('/')
+      .then(() => Toast.error(formatAccountMessage({ id: 'verification.failed', defaultMessage: 'Invalid token' })));
+  }, []);
+
   //verify user's email
+  const verifyUser = useCallback(async () => {
+    if (!token) return;
+    try {
+      const response = await confirm(token as string);
+      if (response.accountId) successRedirect();
+      else errorRedirect();
+    } catch (err) {
+      errorRedirect();
+    }
+  }, [token, confirm, successRedirect, errorRedirect]);
 
   useEffect(() => {
-    const verifyUser = async () => {
-      //successful redirection after verification
-      const successRedirect = () => {
-        router
-          .push('/')
-          .then(() =>
-            Toast.success(formatAccountMessage({ id: 'verification.done', defaultMessage: 'Email verified' })),
-          );
-      };
-
-      //error redirection becaues of invalid token
-      const errorRedirect = () => {
-        router
-          .push('/')
-          .then(() =>
-            Toast.error(formatAccountMessage({ id: 'verification.failed', defaultMessage: 'Invalid token' })),
-          );
-      };
-
-      if (!token) return;
-      try {
-        const response = await confirm(token as string);
-        if (response.accountId) successRedirect();
-        else errorRedirect();
-      } catch (err) {
-        errorRedirect();
-      }
-    };
-
     verifyUser();
-  }, [token, confirm, formatAccountMessage, router]);
+  }, [verifyUser]);
 
   return <></>;
 };
