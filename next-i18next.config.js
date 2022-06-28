71a5510aacdeba95b995f1ad5d718e798eafaa86
Andrius <xcintas@gmail.com>
2022-06-28 16:22:39 +0300

    english is main language

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,14 +3,14 @@ const path = require('path');
 module.exports = {
   // Frontend locales
   i18n: {
-    defaultLocale: 'de',
-    locales: ['de', 'en'],
+    defaultLocale: 'en',
+    locales: ['en', 'de'],
   },
 
   // Backend locales, they will be send to our backend endpoints on every api call, must correspond to frontend locales!!
   i18nForBackend: {
-    'de': 'de_CH',
-    'en': 'en_GB'
+    'en': 'en_GB',
+    'de': 'de_CH'    
   },
   localePath: path.resolve('./public/locales'),
 };
