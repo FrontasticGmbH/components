fa170a855f47f6881dc632291e1a861925232e3c
Carlos Mourullo <carlos@frontastic.cloud>
2022-06-29 12:47:13 +0200

    feat(FP-2185): fixed import of payment Amount

--- frontastic/provider/Frontastic/UseCheckout.ts
+++ frontastic/provider/Frontastic/UseCheckout.ts
@@ -1,4 +1,4 @@
-import { Amount } from '../../../../extensions/payment-adyen/Amount';
+import { Amount } from '../../../../extensions/payment-adyen/Session';
 
 export interface UseCheckout {
   createSession: (amount: Amount, returnUrl: any) => Promise<any>;
