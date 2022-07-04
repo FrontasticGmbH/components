cdce834caba4188d03715bd23c76fe442d68c577
Mina Badrous <minabadrous.vu@gmail.com>
2022-07-04 11:56:44 +0200

    Fix account languages mapping issue

--- helpers/utils/i18n.ts
+++ helpers/utils/i18n.ts
@@ -1,16 +1,7 @@
 export const mapLocaleToMeaningfulFormat = (locale: string) => {
   switch (locale) {
-    case 'de_DE':
-    case 'de_CH':
+    case 'de':
       return { name: 'German' };
-    case 'en_EU':
-    case 'en_GB':
-      return { name: 'English' };
-    case 'nl_NL':
-    case 'nl_BE':
-      return { name: 'Dutch' };
-    case 'fr_CH':
-      return { name: 'French' };
     default:
       return { name: 'English' };
   }
