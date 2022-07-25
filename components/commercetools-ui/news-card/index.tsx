3c69ed9bd034c6d8b09a5d75af3c0426d0b0340e
abdotaker608 <abdotaker607@gmail.com>
2022-07-25 09:40:48 +0200

    Added support for rich text

--- components/commercetools-ui/news-card/index.tsx
+++ components/commercetools-ui/news-card/index.tsx
@@ -1,29 +1,43 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { BLOCKS } from '@contentful/rich-text-types';
 
 export interface Props {
   headline?: string;
   subline?: string;
-  description?: string;
+  description?: Parameters<typeof documentToReactComponents>[0];
   buttonText?: string;
   link?: string;
   image?: { url: string };
 }
 
 const NewsCard: React.FC<Props> = ({ headline, subline, description, link, buttonText, image }) => {
+  const Description = useMemo(
+    () =>
+      documentToReactComponents(description, {
+        renderNode: {
+          [BLOCKS.EMBEDDED_ASSET]: (node) => <img src={node.data.target.fields.file.url} className="my-4" />,
+        },
+      }),
+    [description],
+  );
+
   return (
     <div className="rounded-lg border border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-800">
-      <a href={link}>
+      <a href={link} target="_blank" rel="noreferrer">
         <img className="max-h-[600px] w-full rounded-t-lg object-cover px-5 py-2" src={image?.url} alt="step3" />
       </a>
       <div className="p-5">
-        <a href={link}>
+        <a href={link} target="_blank" rel="noreferrer">
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{headline}</h5>
         </a>
         <div className="mt-1 mb-2 text-xs font-bold uppercase text-teal-700">{subline}</div>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{description}</p>
+        <div className="prose mb-3 dark:prose-invert">{Description}</div>
         {buttonText && (
           <a
             href={link}
+            target="_blank"
+            rel="noreferrer"
             className="inline-flex items-center rounded-lg bg-blue-700 py-2 px-3 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             {buttonText}
