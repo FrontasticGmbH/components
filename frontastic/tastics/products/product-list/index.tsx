2e8ed45f8f67d011c68a62e33ffe4bfd6473d4fc
abdotaker608 <abdotaker607@gmail.com>
2022-08-03 14:34:57 +0200

    Corrected null check for PLP

--- frontastic/tastics/products/product-list/index.tsx
+++ frontastic/tastics/products/product-list/index.tsx
@@ -1,9 +1,9 @@
 import ProductList from 'components/commercetools-ui/products/product-list';
 
 function ProductListTastic({ data }) {
-  if (!data) return <></>;
+  if (!data?.data?.dataSource) return <></>;
 
-  const { items, facets, category, previousCursor, nextCursor, totalItems } = data?.data?.dataSource;
+  const { items, facets, category, previousCursor, nextCursor, totalItems } = data.data.dataSource;
 
   return (
     <ProductList
