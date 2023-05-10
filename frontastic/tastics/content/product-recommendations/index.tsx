03d6559eaeaff6a0b1e60928f83d31c660814af7
King-Hin Leung <>
2023-05-10 17:22:42 +0800

    Simplify the datasource checking for recommended product list

--- frontastic/tastics/content/product-recommendations/index.tsx
+++ frontastic/tastics/content/product-recommendations/index.tsx
@@ -1,11 +1,8 @@
 import ProductList from 'components/commercetools-ui/content/product-recommendations';
 
 const ProductRecommendationsTastic = ({ data }) => {
-  if (!data?.data?.dataSource) return <></>;
-  const productRecommendationsResult = data?.data?.dataSource;
-  const items = productRecommendationsResult.items;
-  const totalItems = productRecommendationsResult.items.length;
-  return <ProductList products={items} totalProducts={totalItems} />;
+  const items = data?.data?.dataSource?.items;
+  return <ProductList products={items} totalProducts={items.length} />;
 };
 
 export default ProductRecommendationsTastic;
