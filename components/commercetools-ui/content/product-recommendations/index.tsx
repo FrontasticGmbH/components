f76497745da32bbb068fdcd3c41dd45308ae7869
King-Hin Leung <>
2023-05-16 16:14:34 +0200

    Add translatable page title

--- components/commercetools-ui/content/product-recommendations/index.tsx
+++ components/commercetools-ui/content/product-recommendations/index.tsx
@@ -3,17 +3,18 @@ import { useFormat } from 'helpers/hooks/useFormat';
 import List from './list';
 
 export interface Props {
+  pageTitle: string;
   products: Product[];
   totalProducts: number;
 }
 
-export default function ProductList({ products, totalProducts }: Props) {
+export default function ProductList({ pageTitle, products, totalProducts }: Props) {
   const { formatMessage: formatProductMessage } = useFormat({ name: 'product' });
 
   return (
     <div className="mt-10 px-1 sm:px-3 lg:px-6">
       <div className="mt-8 gap-16 lg:grid lg:grid-cols-3">
-        <p>Product Recommendations</p>
+        <p>{pageTitle}</p>
         <h6 className="col-span-2 hidden text-right dark:text-light-100 lg:block">
           {`${products.length} ${formatProductMessage({ id: 'items', defaultMessage: 'Items' })} ${totalProducts}`}
         </h6>
--- frontastic/tastics/content/product-recommendations/index.tsx
+++ frontastic/tastics/content/product-recommendations/index.tsx
@@ -1,8 +1,9 @@
 import ProductList from 'components/commercetools-ui/content/product-recommendations';
 
 const ProductRecommendationsTastic = ({ data }) => {
+  const pageTitle = data.pageTitle;
   const items = data?.data?.dataSource?.items;
-  return <ProductList products={items} totalProducts={items.length} />;
+  return <ProductList pageTitle={pageTitle} products={items} totalProducts={items.length} />;
 };
 
 export default ProductRecommendationsTastic;
