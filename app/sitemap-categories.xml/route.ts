014cfeb44fda0dd4f4dc43ad575ba6398a3a19a4
xcintas <xcintas@gmail.com>
2023-10-31 20:47:48 +0200

    commented sitemap generation

--- app/sitemap-categories.xml/route.ts
+++ app/sitemap-categories.xml/route.ts
@@ -1,26 +1,26 @@
-import { Category } from 'shared/types/product/Category';
+//import { Category } from 'shared/types/product/Category';
 //import { Result } from 'shared/types/product/Result';
 import { SiteMapField, generateSiteMap } from 'helpers/sitemap';
-import { sdk } from 'sdk';
+//import { sdk } from 'sdk';
 
 export async function GET() {
-  const locale = 'en';
+  //const locale = 'en';
 
-  const siteUrl = process.env.SITE_URL;
+  //const siteUrl = process.env.SITE_URL;
 
-  sdk.configureForNext(locale);
+  //sdk.configureForNext(locale);
 
   const fields = [] as SiteMapField[];
 
-  let nextCursor: string | undefined;
+  //let nextCursor: string | undefined;
 
-  const extensions = sdk.composableCommerce;
+  //const extensions = sdk.composableCommerce;
 
-  do {
-    /*const response = await sdk.callAction<Result>({
+  /*do {
+    const response = await sdk.callAction<Result>({
       actionName: 'product/queryCategories',
       payload: { cursor: nextCursor, limit: 12 },
-    });*/
+    });
 
     const response = await extensions.product.queryCategories({ cursor: nextCursor, limit: 12 });
 
@@ -36,6 +36,7 @@ export async function GET() {
 
     nextCursor = (!response.isError && response.data.nextCursor) as string;
   } while (nextCursor);
+  */
 
   return new Response(generateSiteMap(fields), {
     headers: {
